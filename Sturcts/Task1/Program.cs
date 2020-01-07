9955870e8f57ee2633e1f50ca5377f0c602b2491
Alexey <alex.agoshkov6@gmail.com>
2020-01-07 22:50:51 +0200

    Task1 Task2
    Task1 Task2

---
+++ Sturcts/Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    struct NoteBook
+    {
+        string _manufacturer;
+        string _model;
+        int _price;
+
+        public NoteBook(string manufacturer, string model, int price)
+        {
+            _manufacturer = manufacturer;
+            _model = model;
+            _price = price;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"{_manufacturer} {_model} {_price}");
+        }
+            
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NoteBook noteBook = new NoteBook("A", "B", 20);
+            noteBook.Display();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sturcts/Task2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    struct Train
+    {
+        string _nameOfCity;
+        int _trainNumber;
+        double _timeStart;
+
+        public Train(string nameOfCity, int trainNumber, double timeStart)
+        {
+            _nameOfCity = nameOfCity;
+            _trainNumber = trainNumber;
+            _timeStart = timeStart;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"{_nameOfCity} {_trainNumber} {_timeStart}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Train[] trains = new Train[8];
+
+            string name;
+            int train;
+            double time;
+
+            for (int i = 0; i < trains.Length; i++)
+            {
+                name = Console.ReadLine();
+                train = Convert.ToInt32(Console.ReadLine());
+                time = Convert.ToDouble(Console.ReadLine());
+
+                trains[i] = new Train(name, train, time);
+            }
+
+            foreach (var item in trains)
+            {
+                item.Display();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
