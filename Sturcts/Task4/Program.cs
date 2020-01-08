ed9c0480a9e066ad197a66d279422b34aac074aa
Alexey <alex.agoshkov6@gmail.com>
2020-01-08 14:02:24 +0200

    Task4
    Task4

--- Sturcts/Task4/Program.cs
+++ Sturcts/Task4/Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace Task4
 {
+    struct Time
+    {
+        public int Days(DateTime date)
+        {
+            int result = 0;
+
+            DateTime dt = new DateTime(date.Year, date.Month, date.Day);
+
+            result = DateTime.Now.Subtract(dt).Days;
+
+            return result;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter DOB (dd/mm): ");
+            string dob = "06/02/"; //Console.ReadLine();
+            dob += DateTime.Now.Year;
+            DateTime dt = DateTime.Parse(dob);
+
+            Time time; 
+
+            Console.WriteLine(time.Days(dt) * -1);
+
+            Console.ReadKey();
         }
     }
 }
